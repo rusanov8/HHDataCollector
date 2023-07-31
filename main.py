2f714bfa03d1123b355863d8bb7cdfcf38b18a20
rusanov8 <rusanov.egor@bk.ru>
2023-07-31 18:35:50 +0700

    fix project

--- main.py
+++ main.py
@@ -28,6 +28,33 @@ def main():
     # Объект класса DBManager для работы с запросами к таблицам
     db_manager = DBManager(params, db_name)
 
+    # Получаем результаты запросов
+    companies_and_vacancies = db_manager.get_companies_and_vacancies_count()
+    all_vacancies = db_manager.get_all_vacancies()
+    avg_salary = db_manager.get_avg_salary()
+    higher_salary_vacancies = db_manager.get_vacancies_with_higher_salary()
+    keyword = 'python'
+    keyword_vacancies = db_manager.get_all_vacancies(keyword)
+
+    # Выводим результаты запросов на экран
+    print("Список всех компаний и количество вакансий:")
+    for company, vacancies_count in companies_and_vacancies:
+        print(f"{company}: {vacancies_count} вакансий")
+
+    print("\nСписок всех вакансий:")
+    for company, title, salary, url in all_vacancies:
+        print(f"Компания: {company}, Вакансия: {title}, Зарплата: {salary}, Ссылка: {url}")
+
+    print("\n Средняя зарплата по вакансиям:", avg_salary)
+
+    print("Вакансии с зарплатой выше средней:")
+    for vacancy in higher_salary_vacancies:
+        print(vacancy)
+
+    print("Вакансии с ключевым словом 'python':")
+    for vacancy in keyword_vacancies:
+        print(vacancy)
+
     # Закрываем подключение
     db_manager.close()
 
